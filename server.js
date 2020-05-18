5616de481f74a2c274049c46615b9490d7e8aef1
Angela Venger <venger@Angelas-MacBook-Pro.local>
2020-05-18 11:22:15 -0700

    connected to MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the ContactKeeper API' })
 );
+
+//Conntect database
+connectDB();
 
 //Define Routes
 app.use('/api/users', require('./routes/users'));
