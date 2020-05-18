17d8140181d922542c3ae8cbd6d29426010bdac3
Angela Venger <venger@Angelas-MacBook-Pro.local>
2020-05-18 14:59:55 -0700

    added middleware for protected route

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,23 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
+
+// @route GET api/auth
+// @desc Get logged in user
+//@access Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 // @route POST api/auth
 // @desc Auth user & get token
@@ -58,12 +72,5 @@ router.post(
     }
   }
 );
-
-// @route GET api/auth
-// @desc Get logged in user
-//@access Private
-router.post('/', (req, res) => {
-  res.send('Log in user');
-});
 
 module.exports = router;
