9bfe06b755d04038ca97ffc2abbd80694d44eb5e
Angela Venger <venger@Angelas-MacBook-Pro.local>
2020-05-26 11:16:13 -0700

    fix couple bugs

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -2,7 +2,7 @@ import React, { Fragment, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import AuthContext from '../../context/auth/authContext';
-import ContactContext from '../../context/contact/contactContextContext';
+import ContactContext from '../../context/contact/contactContext';
 
 const Navbar = ({ title, icon }) => {
   const authContext = useContext(AuthContext);
